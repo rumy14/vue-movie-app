5912c07c8a314f7c2866999c312e970dab435775
rumy14 <unitedit21@gmail.com>
2021-05-10 20:00:45 +0600

    add search...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,49 @@
 <template>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1> <span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', sans-serif;
 
+    &::selection{
+      background: transparentize(#42B883, 0.5);
+    }
+  }
+
+  body{
+    background-color: #35495E;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 16px;
+    background-clip: #2c3d4e;
+    box-shadow: 0 0 6px rgba(0, 0, 0, 0.1);
+  }
+
+  h1{
+    color: #FFF;
+    font-size: 28px;
+
+    span{
+      color: #42b883;
+    }
+  }
 </style>
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="movie-deatail">
+        Details {{ $route.params.id }}
+    </div>
+</template>
+<script>
+
+export default {
+
+}
+</script>
+<style lang="scss">
+.movie-detail {
+  padding: 16px;
+  h2 {
+    color: #FFF;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #FFF;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
+</style>
